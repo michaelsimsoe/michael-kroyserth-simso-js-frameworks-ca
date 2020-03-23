0afde831d8ab46cf4860899d6b9c33704e9987af
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-03-23 23:17:05 +0100

    make api call in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 
 export const Home = () => {
+  // [games, setGames ] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.rawg.io/api/games')
+      .then(res => res.json())
+      .then(data => console.log(data.results));
+  })
   return (
     <p>Home</p>
   )
